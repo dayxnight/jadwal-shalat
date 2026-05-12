feaca3da4109cb19242b11a3a366409448d5cd32
dayxnight <160743101+dayxnight@users.noreply.github.com>
2026-05-12 17:30:49 +0700

    Refactor UI to Material 3, optimize performance, add PWA support, and enhance UX

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'jadwal-shalat-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/index.css',
+  './js/index.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
+  'https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event (Offline First)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
